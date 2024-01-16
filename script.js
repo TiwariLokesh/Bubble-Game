e7be5438f80792d121c3c471449a92a0b17c0f09
Your Name <lokeshtiwari212@gmail.com>
2024-01-16 16:31:24 +0530

    Set Timer done

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+var timer = 6;
 
+//Bubble formation
 function makeBubble(){
     var clutter = "";
     for(var i=1;i<=70;i++){
@@ -8,3 +10,17 @@ function makeBubble(){
     document.querySelector("#pbtm").innerHTML = clutter;
 }
 makeBubble();
+
+function setTimer(){
+    var timerint = setInterval(function(){
+        if(timer>0){
+            timer --;
+            document.querySelector("#timer").textContent = timer;
+        }
+        else{
+            clearTimeout(timerint);
+            document.querySelector("#pbtm").innerHTML = `<div>GAME OVER</div>`
+        }
+    },1000)
+}
+setTimer();
